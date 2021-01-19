7a7943f74bc2ca852c7dd22ae228b3d5d152aa4c
Klinger <kger.matheus@gmail.com>
2021-01-19 11:12:37 -0300

    Capslock function in React

--- pages/contato.js
+++ pages/contato.js
@@ -1,5 +1,13 @@
 function Contato(){
-    return <div>Eae Galera, volte  <a href="/index">Home</a></div>
+    return (<div>Eae Galera, volte  <a href="/index">Home</a>
+            <Capslock texto="Klinger Matheus Vieira da Silva" /></div>)
+}
+
+function Capslock(props) {
+    const value = props.texto
+    const returnText = value.toUpperCase()
+
+    return <p>{returnText}</p>
 }
 
 export default Contato
