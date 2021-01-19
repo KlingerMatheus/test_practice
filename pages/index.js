30f1ff9eb8b57ba1c38126f27e4f7cb4c54a5205
Klinger <kger.matheus@gmail.com>
2021-01-19 14:23:18 -0300

    Teste

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,42 @@
+import { useState } from 'react';
+import { Button, Badge} from '@material-ui/core';
+import DoneAllIcon from '@material-ui/icons/Done';
+
 function Home() {
-    return <div>Now it's updated. <lcase>go to:</lcase> <a href="/contato">Contato</a></div>
+    return (
+    <html>
+        <head>
+        <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        </head>    
+        <body style={{fontFamily:'Roboto'}}>
+            <div>Now it's updated. <a href="/contato">Contato</a>
+                <Contador />
+            </div>
+        </body>
+    </html>
+    )
 }
 
-function Capslock(props) {
-    const value = props.children
-    const valueLCase = value.toUpperCase()
+function Contador() {
+    const [contador,setContador] = useState(0);
 
-    return <Capslock>{valueLCase}</Capslock>
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+
+    return (
+        <div>
+            <div>
+                <Badge badgeContent={contador} color="primary">
+                    <DoneAllIcon />
+                </Badge>
+            </div>
+            <Button variant="contained" color="primary" onClick={adicionarContador}>Adicionar +1</Button>
+        </div>
+    )
 }
 
 export default Home
